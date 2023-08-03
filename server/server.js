293559bd3fe014db9cd4dcc5ca7a65611810dcae
Nehal <nehalcoding@gmail.com>
2023-08-04 00:17:22 +0530

    project complete

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-const PORT = 4000
+import bodyParser from "body-parser";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+import passport from "passport";
+import passportConfig from "./config/passport.js";
+import connect from "./database/mongdb.js";
+import routes from "./routes/index.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
 const app = express();
-app.use(cors);
-mongoose.connect("mongodb+srv://nehaljain:Abcd123@expensetracker.qpitanq.mongodb.net/?retryWrites=true&w=majority")
-.then(()=>console.log("mongodb connection is successful."))
-.catch((err)=>console.error(err));
-app.get("/",(req,res) => {
-    res.send("Hello World");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(passport.initialize());
+passportConfig(passport);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
-app.listen(PORT,()=>{
-    console.log('Server is running at http://localhost:4000');
-})
+app.use("/", routes);
+
+await connect();
+
+app.listen(PORT, () => {
+  console.log("Server is running at http://localhost:4000");
+});
